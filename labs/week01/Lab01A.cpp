050c766bae7a769e5639fcf5510ebe971b7aeb86
root <root@localhost.localdomain>
2026-01-05 22:01:19 +0800

    add week01 Lab01A

--- labs/week01/Lab01A.cpp
+++ labs/week01/Lab01A.cpp
@@ -8,25 +8,36 @@
 std::mutex read_m;
 std::mutex write_m;
 
-void read_file(std::string file, std::fstream &fs) {
+std::streambuf *read_file(std::string file) {
   std::lock_guard<std::mutex> read_lock(read_m);
-  std::fstream(file, std::ios::in | std::ios::binary);
-  fs.read(, streamsize n)
+  std::fstream f{file, std::ios::binary};
+  return f.rdbuf();
 }
 
-void write_file(std::string file) {
+void write_file(std::string file, std::streambuf *buf) {
   write_m.lock();
   try {
-    std::fstream(file, std::ios::out | std::ios::binary);
-  } catch (...) {
+    std::fstream dest{file, std::ios::out | std::ios::binary};
+    dest << buf;
+  } catch (...) { // 异常路径的反例
     throw;
   }
+
   write_m.unlock();
 }
 
 void copy_file(std::string src, std::string dst) {
-  read_file(src);
-  write_file(dst);
+  std::streambuf *buf = read_file(src);
+  write_file(dst, buf);
 }
 
-int main() { std::thread t1(copy_file("src", "dst")) }
+int main() {
+  std::thread t1(copy_file, "src", "dst");
+  std::thread t2(copy_file, "dst", "src");
+  if (t1.joinable()) {
+    t1.join();
+  }
+  if (t2.joinable()) {
+    t2.join();
+  }
+}
