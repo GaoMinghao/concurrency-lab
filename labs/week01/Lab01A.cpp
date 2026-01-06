38cdae181a6c18bb693129f9e74609fb52fd83ad
root <root@localhost.localdomain>
2026-01-06 10:45:07 +0800

    fix: add thread_guard and fix uaf in file read

--- labs/week01/Lab01A.cpp
+++ labs/week01/Lab01A.cpp
@@ -1,25 +1,44 @@
 // Lab 01A：`thread_guard` + 异常路径反例
-#include <cwchar>
-#include <fstream>
+#include <iostream>
 #include <mutex>
 #include <string>
 #include <thread>
 
+class thread_guard {
+public:
+  explicit thread_guard(std::thread &t_) : t(t_) {}
+  ~thread_guard() {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+  thread_guard(thread_guard &other) = delete;
+  thread_guard &operator=(thread_guard &other) = delete;
+
+private:
+  std::thread &t;
+};
+
 std::mutex read_m;
 std::mutex write_m;
 
-std::streambuf *read_file(std::string file) {
+std::string read_file(std::string file) {
   std::lock_guard<std::mutex> read_lock(read_m);
-  std::fstream f{file, std::ios::binary};
-  return f.rdbuf();
+  std::string content = "Data from " + file;
+  std::cout << "[Read] Read data from " << file << std::endl;
+  return content;
 }
 
-void write_file(std::string file, std::streambuf *buf) {
+void write_file(std::string file, std::string &data) {
   write_m.lock();
   try {
-    std::fstream dest{file, std::ios::out | std::ios::binary};
-    dest << buf;
+    std::cout << "[Write] Writing to " << file << ": " << data << std::endl;
+
+    if (file.find("fail") != std::string::npos) {
+      throw std::runtime_error("Write failed");
+    }
   } catch (...) { // 异常路径的反例
+    std::cout << "[Error] Exception caught, but forgot to unlock!" << std::endl;
     throw;
   }
 
@@ -27,17 +46,17 @@ void write_file(std::string file, std::streambuf *buf) {
 }
 
 void copy_file(std::string src, std::string dst) {
-  std::streambuf *buf = read_file(src);
-  write_file(dst, buf);
+  std::string data = read_file(src);
+  write_file(dst, data);
 }
 
 int main() {
-  std::thread t1(copy_file, "src", "dst");
-  std::thread t2(copy_file, "dst", "src");
-  if (t1.joinable()) {
-    t1.join();
-  }
-  if (t2.joinable()) {
-    t2.join();
+  try {
+    std::thread t1(copy_file, "src", "dst");
+    thread_guard g1(t1);
+    std::thread t2(copy_file, "data", "fail");
+    thread_guard g2(t2);
+  } catch (const std::exception &e) {
+    std::cout << "Main caught exception: " << e.what() << std::endl;
   }
 }
